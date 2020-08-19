c98267ea599de4f8910242f5aed8ac6983535086
zhongzhen.hzz <zhongzhen.hzz@alibaba-inc.com>
2020-08-19 12:30:56 +0800

    husky

--- src/components/demo/index.tsx
+++ src/components/demo/index.tsx
@@ -1,8 +1,6 @@
 import React, { FC } from 'react'
 import s from './index.scss'
 
-const Demo: FC<{ demo?: string }> = () => {
-  return <div className={s.demo}>demo</div>
-}
+const Demo: FC<{ demo?: string }> = () => <div className={s.demo}>demo</div>
 
 export default Demo
